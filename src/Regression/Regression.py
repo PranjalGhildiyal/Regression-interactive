e6a6202ee112d3139c27a89334691d5a4c02e4d3
Pranjal <epe18029@rgipt.ac.in>
2023-08-11 09:48:20 +0530

    modifying dataclasses

--- src/Regression/Regression.py
+++ src/Regression/Regression.py
@@ -5,7 +5,7 @@ from sklearn.metrics import r2_score
 from sklearn.metrics import mean_squared_error 
 from sklearn.metrics import mean_absolute_percentage_error   
 from sklearn.metrics import mean_absolute_error
-from dataclasses import make_dataclass
+from dataclasses import make_dataclass, field
 import inspect
 from ...config.model_types import ModelAssignment
 
@@ -62,16 +62,19 @@ class Regression:
         return (score, mse, rmse, mape, mae)
     
     @staticmethod
-    def get_configs()->dict:
+    def get_all_configs()->dict:
         return ModelAssignment.regression
     
     @staticmethod
-    def get_model(model_type:str)->ModelAssignment.RegressorModelType:
-        available_models= Regression.get_configs()
-        if model_type in available_models.keys():
-            return available_models[model_type]
-        else:
-            raise InvalidModelException
+    def get_configs(model_types:list)->ModelAssignment.RegressorModelType:
+        available_models= Regression.get_all_configs()
+        models={}
+        for model_type in model_types:
+            if model_type in available_models.keys():
+                models[model_type] = available_models[model_type]
+            else:
+                raise InvalidModelException
+        return models
         
     @staticmethod
     def get_all_model_names()->list:
@@ -82,25 +85,29 @@ class Regression:
         return list(Regression.get_configs().values())
 
     @staticmethod
-    def get_extended_configs()->list:
+    def get_extended_configs(model_types:list)->list:
+
         knowledge= {}
-        model_assignment= Regression.get_configs()
+        model_assignment= Regression.get_configs(model_types)
         for model in model_assignment.keys():
             knowledge[model] = {}
             signature = inspect.signature(model_assignment[model].__init__)
             for name, param in signature.parameters.items():
                 #Iterating over parameters of a single model
                 if (name != "self") & (param.default != inspect.Parameter.empty):
-                    knowledge[model][name]= type(param.default)
+                    knowledge[model][name] = {}
+                    knowledge[model][name]['type']= type(param.default)
+                    knowledge[model][name]['default'] = param.default
         return knowledge
 
     @staticmethod
-    def get_dataclasses()->dict:
+    def get_dataclasses(model_types:list)->dict:
+
         data_classes= {}
-        ext_configs= Regression.get_extended_configs()
+        ext_configs= Regression.get_extended_configs(model_types)
         
         for model in ext_configs.keys():
-            fields = [(name, param_type) for name, param_type in ext_configs[model].items()]
+            fields = [(param, ext_configs[model][param]['type'], field(default=ext_configs[model][param]['default'])) for param in ext_configs[model].keys()]
             data_classes[model] = make_dataclass(model, fields)
         
         return data_classes
