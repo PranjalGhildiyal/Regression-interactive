8ca880d59cfb9eaa3bbed651502cce6ba012fc55
Pranjal <epe18029@rgipt.ac.in>
2023-08-13 19:01:30 +0530

    ML_visualizer changes

--- src/Panel/ML_visualizer.py
+++ src/Panel/ML_visualizer.py
@@ -9,24 +9,6 @@ class Visualize_ML:
 
         self.data= Visualize_ML.cleaan_dataset(data)
 
-        # Taking user input for Datetime column
-        self.column_verification = False
-        while not self.column_verification:
-            self.datetime_column = input('Datetime column(press enter if not present):')
-            if self.datetime_column in (list(self.dataset.columns) + ['']):
-                self.column_verification = True
-
-        if self.datetime_column != '':
-            self.dataset[self.datetime_column] = pd.to_datetime(self.dataset[self.datetime_column])
-            max_datetime = self.dataset[self.datetime_column].max()
-            min_datetime = self.dataset[self.datetime_column].min()
-            self.dataset = self.dataset.set_index(self.datetime_column)
-            self.properties.remove(self.datetime_column)
-            
-            self.datetime_picker = pn.widgets.DatetimeRangePicker(value=(min_datetime, min_datetime),
-                                            start=min_datetime.date(), end=max_datetime.date(),
-                                            military_time=False, name='Datetime Range')
-            self.datetime_picker.param.watch(self.__update_timestamps, 'value')
 
 
 
