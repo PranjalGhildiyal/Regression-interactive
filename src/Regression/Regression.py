c0ee4c05afe012f7dead1dd65a9b856953d9f03a
Pranjal <epe18029@rgipt.ac.in>
2023-08-20 04:35:34 +0530

    regression-changes

--- src/Regression/Regression.py
+++ src/Regression/Regression.py
@@ -16,27 +16,27 @@ class Regression:
     def __init__(self, data:pd.DataFrame, target_column:str, train_size:float=0.6, test_size:float=0.2, datetime_column:str=None, random_state:int=0):
 
         if datetime_column== None:
-            data = data.reset_index(drop=True).reset_index()
-            datetime_column= 'index'
+            data= data.sort_index(ascending=True)
+        else:
+            data= data.sort_values(by=datetime_column, ascending=True).set_index(datetime_column)
         self.data= data
-        self.datetime_column = datetime_column
         self.target_column = target_column
         self.model_archive= ModelArchive().regression
 
         # Dropping nan values
-        data= data.dropna()
+        self.data= self.data.dropna()
 
         # Raising exception if data is not present
-        if data.shape[0] == 0:
+        if self.data.shape[0] == 0:
             raise NoDataPresentException
         
-        # sorting values accoriding to datetime
-        data = data.sort_values(by = self.datetime_column, ascending = True)
-        self.data_test = data.tail(int(test_size*len(data)))
 
-        data = data.drop(self.data_test.index)
-        self.y = data[target_column]
-        self.X = data.drop(target_column, axis = 1)
+        self.data_test = self.data.tail(int(test_size*len(self.data)))
+
+        self.data = self.data.drop(self.data_test.index)
+        print(self.data.columns)
+        self.y = self.data[self.target_column]
+        self.X = self.data.drop(self.target_column, axis = 1)
         self.X_test = self.data_test.drop(target_column, axis = 1)
         self.y_test = self.data_test[target_column]
         self.data_for_graph= {}
@@ -45,11 +45,9 @@ class Regression:
         self.random_state = random_state
         
         self.x_train, self.x_valid, self.y_train, self.y_valid = train_test_split(self.X, self.y, train_size = self.train_size, random_state = self.random_state)
-        self.x_valid1 = self.x_valid.drop(self.datetime_column, axis = 1)
-        self.x_train1 = self.x_train.drop(self.datetime_column, axis = 1)
     
     @staticmethod
-    def evaluate(test:pd.Series, preds: pd.Series, process:str)->tuple:
+    def evaluate(test:pd.Series, preds: pd.Series, process:str= 'Process')->tuple:
         score=r2_score(test,preds)
         mse = mean_squared_error(test,preds)
         rmse = np.sqrt(mean_squared_error(test,preds))
--- src/Regression/Regressors.py
+++ src/Regression/Regressors.py
@@ -5,7 +5,7 @@ import mlflow
 import optuna
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import RepeatedKFold 
-
+import re
 from src.Regression.Regression import Regression
 from config.model_types import RegressionModelAssignment
 
@@ -21,42 +21,49 @@ class Regressor(Regression):
                 test_size:float=0.2, 
                 datetime_column:str=None,
                 random_state:int=0,
-                **kwargs)->'Regressor':
+                hyperparameters:dict={}):
 
         super().__init__(data, target_column, train_size, test_size, datetime_column, random_state)
-        self.model_type=RegressionModelAssignment(model_type).model
-        session = mlflow.start_run()
+        self.model=RegressionModelAssignment(model_type).model
+        self.model_type= model_type
         
+
+        self.hyperparameters_new = {}
+        for hyperparameter in hyperparameters:
+            self.hyperparameters_new[hyperparameter] = hyperparameters[hyperparameter]['default']
+
+    def fit_model(self)->'Regressor':
+
+        session = mlflow.start_run()
         with session :
             
             ## Building a model
-            self.model = self.model_type(**kwargs)
-            self.model.fit(self.x_train1,self.y_train)
+            self.model = self.model(**self.hyperparameters_new)
+            self.model.fit(self.x_train,self.y_train)
             
             ## On Training Data:
-            y_pred_train = self.model.predict(self.x_train1)
+            y_pred_train = self.model.predict(self.x_train)
             (self.train_score, self.train_mse, self.train_rmse, self.train_mape, self.train_mae) = Regression.evaluate(self.y_train, y_pred_train, 'Train')
-            self.data_for_graph['Train'] = pd.DataFrame({'Actual': self.y_train, 'Predictions': y_pred_train, 'Date_time': self.x_train[self.datetime_column]}).sort_values(by = 'Date_time', ascending=True).reset_index(drop = True)
+            self.data_for_graph['Train'] = pd.DataFrame({'Actual': self.y_train, 'Predictions': y_pred_train, 'Date_time': self.x_train.index}).sort_values(by = 'Date_time', ascending=True).reset_index(drop = True)
 
             ## On Validation Data:
-            y_pred_valid = self.model.predict(self.x_valid1)
+            y_pred_valid = self.model.predict(self.x_valid)
             (self.valid_score, self.valid_mse, self.valid_rmse, self.valid_mape, self.valid_mae) = Regression.evaluate(self.y_valid, y_pred_valid, 'Validation')
-            self.data_for_graph['Validation'] = pd.DataFrame({'Actual': self.y_valid, 'Predictions': y_pred_valid, 'Date_time': self.x_valid[self.datetime_column]}).sort_values(by = 'Date_time', ascending=True).reset_index(drop = True)
+            self.data_for_graph['Validation'] = pd.DataFrame({'Actual': self.y_valid, 'Predictions': y_pred_valid, 'Date_time': self.x_valid.index}).sort_values(by = 'Date_time', ascending=True).reset_index(drop = True)
 
             ## On Test data: 
-            y_pred_test = self.model.predict(self.X_test.drop(self.datetime_column, axis = 1))
+            y_pred_test = self.model.predict(self.X_test)
             (self.test_score, self.test_mse, self.test_rmse, self.test_mape, self.test_mae) = Regression.evaluate(self.y_test, y_pred_test, 'Test')
-            self.data_for_graph['Test'] = pd.DataFrame({'Actual': self.y_test, 'Predictions': y_pred_test, 'Date_time': self.X_test[self.datetime_column]}).sort_values(by = 'Date_time', ascending=True).reset_index(drop = True)
+            self.data_for_graph['Test'] = pd.DataFrame({'Actual': self.y_test, 'Predictions': y_pred_test, 'Date_time': self.X_test.index}).sort_values(by = 'Date_time', ascending=True).reset_index(drop = True)
 
             
 
             # Log parameters and metrics to MLflow
             mlflow.log_param("train_size", self.train_size)
-            mlflow.log_param("random_state", self.random_state)
-            mlflow.log_param("model_type", model_type)
+            mlflow.log_param("random_state_train_test_split", self.random_state)
+            mlflow.log_param("model_type", self.model_type)
             params= self.model.get_params()
-            for param in params.keys():
-                mlflow.log_params(param, params[param])
+            mlflow.log_params(params)
 
             mlflow.log_metric("r2_score_train", self.train_score)
             mlflow.log_metric("mse_train", self.train_mse)
@@ -86,35 +93,63 @@ class Optimizer(Regression):
     def __init__(self, 
                 data:pd.DataFrame, 
                 target_column:str,
+                scoring:str,
+                cv:dict,
                 model_type:str='LinearRegression',
                 train_size:float=0.6,
                 test_size:float=0.2, 
                 datetime_column:str=None,
                 random_state:int=0,
-                scoring:str,
-                cv:dict,
                 buttons:dict=None,
                 n_trials:int=100,
-                **kwargs)->'Regressor':
+                **hyperparameters):
         
         super().__init__(data, target_column, train_size, test_size, datetime_column, random_state)
+        self.model=RegressionModelAssignment(model_type).model
+        self.hyperparameters= hyperparameters
+        self.scoring= scoring
+        self.n_trials= n_trials
+        self.cv= cv
+        self.model_type= model_type
+
+    def optimize(self)->'Optimizer':
 
         def objective(trial):
-            hyperparameters= {}
-            for i in kwargs:
-                hyperparameters[i] = 
-            n_estimators = trial.suggest_int('n_estimators', n_estimators_range[0], n_estimators_range[1])
-            max_depth = int(trial.suggest_loguniform('max_depth', max_depth_range[0], max_depth_range[1]))
-            regressor = self.model_type(n_estimators = n_estimators,  max_depth = max_depth)
-            return np.absolute(sklearn.model_selection.cross_val_score(regressor, self.X, self.y, scoring=scoring, n_jobs=-1, cv=cv['n_splits'])).mean()
+            trial_hyperparameters= {}
+            for i in self.hyperparameters:
+                type= self.hyperparameters[i]['type']
+                value=self.hyperparameters[i]['default']
+                result = []
+                for item in value:
+                    if isinstance(item, list):
+                        result.extend(range(item[0], item[1] + 1))
+                    else:
+                        result.append(item)
+                result= list(set(result))
+
+                hyperparameter_range= [max(result), min(result)]
+
+                if (type == type(None)) or (type == str):
+                    type = Optimizer.classify_math_number(value)
+
+
+                if type == type(int):
+                    trial_hyperparameters[i] = trial.suggest_int(i, hyperparameter_range[0], hyperparameter_range[1])
+                if type == type(float):
+                    trial_hyperparameters[i] = trial.suggest_float(i, hyperparameter_range[0], hyperparameter_range[1])
+                if type == type(str):
+                    pass
+
+            regressor = self.model(**trial_hyperparameters)
+            return np.absolute(sklearn.model_selection.cross_val_score(regressor, self.X, self.y, scoring=self.scoring, n_jobs=-1, cv=self.cv['n_splits'])).mean()
 
         # OPTUNA APPLICATION
-        if 'score' in scoring:
+        if 'score' in self.scoring:
             study = optuna.create_study(direction='maximize')
         else:
             study = optuna.create_study(direction='minimize')
 
-        study.optimize(objective, n_trials=n_trials)
+        study.optimize(objective, n_trials=self.n_trials)
         trial = study.best_trial
         self.optimization_traectory= optuna.visualization.plot_optimization_history(study)
 
@@ -122,41 +157,40 @@ class Optimizer(Regression):
         session = mlflow.start_run()
         with session :
             # Building the model
-            self.model = self.model_type(n_estimators=trial.params['n_estimators'], max_depth= trial.params['max_depth'])
+            self.model = self.model(**trial.params)
 
             # Cross Validation
-            cv = RepeatedKFold(n_splits=cv['n_splits'], n_repeats=cv['n_repeats'], random_state=cv['random_state'])
-            scores = cross_val_score(self.model, self.X, self.y, scoring=scoring, cv=cv, n_jobs=cv['n_jobs'])
+            CV = RepeatedKFold(n_splits=self.cv['n_splits'], n_repeats=self.cv['n_repeats'], random_state=self.cv['random_state'])
+            scores = cross_val_score(self.model, self.X, self.y, scoring=self.scoring, cv=CV, n_jobs=self.cv['n_jobs'])
             scores = np.absolute(scores)
-            mlflow.log_metric("Cross Validation: mean_{}".format(scoring), scores.mean())
-            mlflow.log_metric("Cross Validation: std_{}".format(scoring), scores.std())
+            mlflow.log_metric("Cross Validation: mean_{}".format(self.scoring), scores.mean())
+            mlflow.log_metric("Cross Validation: std_{}".format(self.scoring), scores.std())
             
 
             # Model fitting
-            self.model.fit(self.x_train1, self.y_train)
-        
+            self.model.fit(self.x_train,self.y_train)
+            
             ## On Training Data:
-            y_pred_train = self.model.predict(self.x_train1)
+            y_pred_train = self.model.predict(self.x_train)
             (self.train_score, self.train_mse, self.train_rmse, self.train_mape, self.train_mae) = Regression.evaluate(self.y_train, y_pred_train, 'Train')
-            self.data_for_graph['Train'] = pd.DataFrame({'Actual': self.y_train, 'Predictions': y_pred_train, 'Date_time': self.x_train[self.datetime_column]}).sort_values(by = 'Date_time', ascending=True).reset_index(drop = True)
+            self.data_for_graph['Train'] = pd.DataFrame({'Actual': self.y_train, 'Predictions': y_pred_train, 'Date_time': self.x_train.index}).sort_values(by = 'Date_time', ascending=True).reset_index(drop = True)
 
             ## On Validation Data:
-            y_pred_valid = self.model.predict(self.x_valid1)
+            y_pred_valid = self.model.predict(self.x_valid)
             (self.valid_score, self.valid_mse, self.valid_rmse, self.valid_mape, self.valid_mae) = Regression.evaluate(self.y_valid, y_pred_valid, 'Validation')
-            self.data_for_graph['Validation'] = pd.DataFrame({'Actual': self.y_valid, 'Predictions': y_pred_valid, 'Date_time': self.x_valid[self.datetime_column]}).sort_values(by = 'Date_time', ascending=True).reset_index(drop = True)
+            self.data_for_graph['Validation'] = pd.DataFrame({'Actual': self.y_valid, 'Predictions': y_pred_valid, 'Date_time': self.x_valid.index}).sort_values(by = 'Date_time', ascending=True).reset_index(drop = True)
 
             ## On Test data: 
-            y_pred_test = self.model.predict(self.X_test.drop(self.datetime_column, axis = 1))
+            y_pred_test = self.model.predict(self.X_test)
             (self.test_score, self.test_mse, self.test_rmse, self.test_mape, self.test_mae) = Regression.evaluate(self.y_test, y_pred_test, 'Test')
-            self.data_for_graph['Test'] = pd.DataFrame({'Actual': self.y_test, 'Predictions': y_pred_test, 'Date_time': self.X_test[self.datetime_column]}).sort_values(by = 'Date_time', ascending=True).reset_index(drop = True)
+            self.data_for_graph['Test'] = pd.DataFrame({'Actual': self.y_test, 'Predictions': y_pred_test, 'Date_time': self.X_test.index}).sort_values(by = 'Date_time', ascending=True).reset_index(drop = True)
 
 
             # Log parameters and metrics to MLflow
             mlflow.log_param("train_size", self.train_size)
-            mlflow.log_param("random_state", self.random_state)
-            mlflow.log_param("model_type", "Random Forest Regressor")
-            for param in trial.param.keys():
-                mlflow.log_params(param, trial.params[param])
+            mlflow.log_param("random_state_train_test_split", self.random_state)
+            mlflow.log_param("model_type", self.model_type)
+            mlflow.log_params(trial.params)
 
             mlflow.log_metric("r2_score_train", self.train_score)
             mlflow.log_metric("mse_train", self.train_mse)
@@ -177,3 +211,12 @@ class Optimizer(Regression):
             mlflow.log_metric("mae_test", self.train_mae)
 
             return self 
+    
+    @staticmethod
+    def classify_math_number(input_str):
+        if re.match(r'^[+-]?\d+$', input_str):
+            return type(int)
+        elif re.match(r'^[+-]?\d+\.\d*$', input_str):
+            return type(float)
+        else:
+            return type(str)
